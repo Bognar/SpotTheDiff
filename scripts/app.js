4751b7585ca9ca4cf1ecb3490ee4da7afb8c8679
Bognar <josip.bognar@optinet.hr>
2019-02-09 14:02:16 +0100

    fixing scripts

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('plunker', []);
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.name = 'World';
+    $scope.preostalo = 5;
+    $scope.box1 = function () {
+        $scope.preostalo--;
+        $scope.skriveno1 = true;
+    }
+    $scope.box2 = function () {
+        $scope.preostalo--;
+        $scope.skriveno2 = true;
+    }
+    $scope.box3 = function () {
+        $scope.preostalo--;
+        $scope.skriveno3 = true;
+    }
+    $scope.box4 = function () {
+        $scope.preostalo--;
+        $scope.skriveno4 = true;
+    }
+    $scope.box5 = function () {
+        $scope.preostalo--;
+        $scope.skriveno5 = true;
+    }
+});
